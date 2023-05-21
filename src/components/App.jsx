da45459f8cf98f7b637c4b7597df1aa0399b6cb1
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2023-05-21 22:22:38 +0300

    update 1.2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import { Component } from 'react';
-
 export class App extends Component {
   state = {
     good: 0,
@@ -7,37 +6,74 @@ export class App extends Component {
     bad: 0,
   };
 
-  countTotalFeedback() {}
+  getGoodFeedbackHandler = () => {
+    this.setState(state => ({ good: state.good + 1 }));
+  };
 
-  countPositiveFeedbackPercentage() {}
+  getNeutralFeedbackHandler = () => {
+    this.setState(state => ({ neutral: state.neutral + 1 }));
+  };
+
+  getBadFeedbackHandler = () => {
+    this.setState(state => ({ bad: state.bad + 1 }));
+  };
+
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  }
+
+  countPositiveFeedbackPercentage() {
+    const { good, neutral, bad } = this.state;
+    const feedbacks = good + neutral + bad;
+    return feedbacks ? ((good / feedbacks) * 100).toFixed() : 0;
+  }
   render() {
-    return (
-      <section>
-        <strong>Please leave feedback</strong>
-        <ul>
-          <li>
-            <button type="button">Good</button>
-          </li>
-          <li>
-            <button type="button">Neutral</button>
-          </li>
-          <li>
-            <button type="button">Bad</button>
-          </li>
-        </ul>
-        <h1>Statistics</h1>
-        <ul>
-          <li>
-            Good:<span></span>
-          </li>
-          <li>
-            Neutral:<span></span>
-          </li>
-          <li>
-            Bad:<span></span>
-          </li>
-        </ul>
-      </section>
+    const { good, neutral, bad } = this.state;
+    <div>
+        {' '}
+        <section>
+          <strong>Please leave feedback</strong>
+          <ul>
+            <li>
+              <button type="button" onClick={this.getGoodFeedbackHandler}>
+                Good
+              </button>
+            </li>
+            <li>
+              <button type="button" onClick={this.getNeutralFeedbackHandler}>
+                Neutral
+              </button>
+            </li>
+            <li>
+              <button type="button" onClick={this.getBadFeedbackHandler}>
+                Bad
+              </button>
+            </li>
+          </ul>
+        </section>
+        <section>
+          <h1>Statistics</h1>
+          <ul>
+            <li>
+              Good:<span>{good}</span>
+            </li>
+            <li>
+              Neutral:<span>{neutral}</span>
+            </li>
+            <li>
+              Bad:<span>{bad}</span>
+            </li>
+            <li>
+              Total:<span>{this.countTotalFeedback()}</span>
+            </li>
+            <li>
+              Positive feedbacks:
+              <span>{this.countPositiveFeedbackPercentage()}%</span>
+            </li>
+          </ul>
+        </section>
+      </div>
     );
   }
 }
